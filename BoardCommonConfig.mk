3eee0bbd28252fea8932141a016c59f347ece2ae
Jason Lu <jasonbangbang@gmail.com>
2017-01-13 23:05:53 -0600

    Rebrand to Lineage
    Change-Id: I534425ab98f25e4586e17d366addc87cf4d14054
    Signed-off-by: Jason Lu <jasonbangbang@gmail.com>

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -34,9 +34,9 @@ TARGET_NEEDS_TEXT_RELOCATIONS := true
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 ifeq ($(TARGET_VOICE_TECH), cdma)
-TARGET_KERNEL_CONFIG := cyanogenmod_t0ltecdma_defconfig
+TARGET_KERNEL_CONFIG := lineageos_t0ltecdma_defconfig
 else
-TARGET_KERNEL_CONFIG := cyanogenmod_t0lte_defconfig
+TARGET_KERNEL_CONFIG := lineageos_t0lte_defconfig
 endif
 
 # Recovery
