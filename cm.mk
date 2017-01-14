8ea52bda957bfed69b3de71c1d4350abc1455ea4
Jason Lu <jasonbangbang@gmail.com>
2017-01-13 23:25:05 -0600

    Remove enhanced NFC
    The required files no longer exist, breaking build.
    
    Change-Id: Idf7df1f27677e3d813e579433f5a4f235f7c8149
    Signed-off-by: Jason Lu <jasonbangbang@gmail.com>

--- cm.mk
+++ cm.mk
@@ -8,9 +8,6 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit device configuration
 $(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
