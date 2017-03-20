50bb40613661cc04b954fe6e59bdf8f9154ea351
Jason Lu <jasonbangbang@gmail.com>
2017-03-19 23:09:12 -0500

    t0lte: Add init_t0lte and define TARGET_UNIFIED_DEVICE
    t0lte is a unified device, containing all LTE/CDMA variants of the Note 2.
    Some of the devices are t0lteatt, t0ltespr, t0ltetmo, and t0lte.
    
    Instead of having a generic build for all variants that ignore the
    differences for each variant, we should set individual properties for
    each variant.
    
    Currently, we detect which variant by grabbing the bootloader string. The
    first four characters of the bootloader string is unique to each variant
    yet remains same across bootloader versions, meaning that we can use this
    to identify the variant.
    
    Change-Id: I5e38304eee90b1a9411067829fa3c089e1768f8d
    Signed-off-by: Jason Lu <jasonbangbang@gmail.com>

---
+++ init/init_t0lte.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string>
+
+#include "vendor_init.h"
+#include "property_service.h"
+#include "log.h"
+#include "util.h"
+
+void vendor_load_properties()
+{
+    std::string bootloader = property_get("ro.bootloader");
+    if (bootloader.find("I317") != std::string::npos) {
+        /* SGH-i317 (AT&T) */
+        property_set("ro.product.model", "SGH-I317");
+        property_set("ro.product.device", "t0lteatt");
+        property_set("ro.build.description", "t0lteuc-user 4.4.2 KOT49H I317UCUCNJ1 release-keys");
+        property_set("ro.build.fingerprint", "samsung/t0lteuc/t0lteatt:4.4.2/KOT49H/I317UCUCNJ1:user/release-keys");
+        property_set("ro.build.product", "t0lteatt");
+    else if (bootloader.find("T889") != std::string::npos) {
+        /* SGH-T889 (T-Mobile) */
+        property_set("ro.product.model", "SGH-T889");
+        property_set("ro.product.device", "t0ltetmo");
+        property_set("ro.build.description", "t0ltetmo-user 4.3 JSS15J T889UVUCMK7 release-keys");
+        property_set("ro.build.fingerprint", "samsung/t0ltetmo/t0ltetmo:4.3/JSS15J/T889UVUCMK7:user/release-keys");
+        property_set("ro.build.product", "t0ltetmo");
+    } else {
+        /* Fall back to N7105 */
+        property_set("ro.product.model", "GT-N7105");
+        property_set("ro.product.device", "t0lte");
+        property_set("ro.build.description", "t0ltexx-user 4.4.2 KOT49H N7105XXUFOB2 release-keys");
+        property_set("ro.build.fingerprint", "samsung/t0ltexx/t0lte:4.4.2/KOT49H/N7105XXUFOB2:user/release-keys");
+        property_set("ro.build.product", "t0lte");
+    }
+    std::string device = property_get("ro.product.device");
+    INFO("Setting device to %s\n", device.c_str());
+}
--- lineage.mk
+++ lineage.mk
@@ -17,6 +17,3 @@ PRODUCT_NAME := lineage_t0lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N7105
 PRODUCT_MANUFACTURER := samsung
-
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lte TARGET_DEVICE=t0lte BUILD_FINGERPRINT="samsung/t0ltexx/t0lte:4.4.2/KOT49H/N7105XXUFOB2:user/release-keys" PRIVATE_BUILD_DESC="t0ltexx-user 4.4.2 KOT49H N7105XXUFOB2 release-keys"
