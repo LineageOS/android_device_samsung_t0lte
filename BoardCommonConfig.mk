aeb0d0375d6647d3ef28fca3a682bec63aafa6c3
Filip Blagojevic <filiprrs@gmail.com>
2017-03-31 16:28:55 +0000

    Revert "Revert "t0lte: allow ROM to be flashed to t0ltektt""
    This reverts commit be5916b6a55aeb610f39ea744a2c76db7a9874fb.
    
    Change-Id: I6c8fa144a55342378fb577e6c565eece4ff27c78

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -61,7 +61,7 @@ BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := mdm9x35
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := t0lte,t0ltexx,GT-N7105,t0ltedv,GT-N7105T,t0lteatt,SGH-I317,t0ltetmo,SGH-T889,t0ltecan,t0ltevl,SGH-I317M,t0lteskt
+TARGET_OTA_ASSERT_DEVICE := GT-N7105,GT-N7105T,SGH-I317,SGH-I317M,SGH-T889,t0lte,t0lteatt,t0ltecan,t0ltedv,t0ltektt,t0lteskt,t0ltetmo,t0ltevl,t0ltexx
 
 # Selinux
 BOARD_SEPOLICY_DIRS += device/samsung/t0lte/selinux
